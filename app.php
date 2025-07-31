b983c6237ad4a588e79080305b60bcfbbf96817d
jounouchi666 <trueguy666@gmail.com>
2025-08-01 03:57:09 +0900

    できた

--- app.php
+++ app.php
@@ -1,3 +1,34 @@
+<?php 
+    session_start();
+
+    // 初期化
+    if (empty($_SESSION['todos'])) {
+        $_SESSION['todos'] = [];
+    }
+
+    // 登録
+    if (isset($_POST['add']) && !empty($_POST['title'])) {
+        $_SESSION['todos'][uniqid()] = strip_tags($_POST['title']);
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit;
+    }
+
+    // 編集データの保存
+    if (isset($_POST['save']) && isset($_POST['id']) && isset($_POST['title'])) {
+        $_SESSION['todos'][$_POST['id']] = strip_tags($_POST['title']);
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit;
+    }
+
+    // 削除
+    if (isset($_POST['delete'])) {
+        unset($_SESSION['todos'][$_POST['id']]);
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit;
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -8,54 +39,56 @@
 </head>
 <body>
     <main>
-        <h1>TODO</h1>
+        <!-- 編集モード -->
+        <?php if (isset($_POST['update']) && !empty($_POST['id']) && isset($_SESSION['todos'][$_POST['id']]) ): ?>
+            <h1>TODO<span class="edit">編集</span></h1>
 
-        <form action="" method="POST" id="add-form">
-            <input type="text" name="title" placeholder="TODOを入力" required>
-            <button class="button-add" type="submit">登録</button>
-        </form>
+            <div class="edit-form">
+                <form action="" method="POST" id="input-form">
+                    <input type="hidden" name="id" value="<?php echo htmlspecialchars($_POST['id']) ?>">
+                    <input
+                        type="text"
+                        name="title"
+                        value="<?php echo htmlspecialchars($_SESSION['todos'][$_POST['id']]) ?>"
+                        required
+                    >
+                    <button class="button-add" type="submit" name="save">保存</button>
+                </form>
+                <button
+                    class="button-cancel"
+                    type="button"
+                    onclick="location.href='<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>'"
+                >キャンセル</button>
+            </div>
+        <!-- 通常表示 -->
+        <?php else: ?>
+            <h1>TODO</h1>
 
-        <ul class="TODO-items">
-            <li class="TODO-item">
-                <div class="TODO-title">タイトル1</div>
-                <div class="nav-container">
-                    <form action="" method="POST">
-                        <input type="hidden" name="id" value="">
-                        <button class="button-update" type="submit">編集</button>
-                    </form>
-                    <form action="" method="POST">
-                        <input type="hidden" name="id" value="">
-                        <button class="button-delete" type="submit">削除</button>
-                    </form>
-                </div>
-            </li>
-            <li class="TODO-item">
-                <div class="TODO-title">タイトル2</div>
-                <div class="nav-container">
-                    <form action="" method="POST">
-                        <input type="hidden" name="id" value="">
-                        <button class="button-update" type="submit">編集</button>
-                    </form>
-                    <form action="" method="POST">
-                        <input type="hidden" name="id" value="">
-                        <button class="button-delete" type="submit">削除</button>
-                    </form>
-                </div>
-            </li>
-            <li class="TODO-item">
-                <div class="TODO-title">タイトル3</div>
-                <div class="nav-container">
-                    <form action="" method="POST">
-                        <input type="hidden" name="id" value="">
-                        <button class="button-update" type="submit">編集</button>
-                    </form>
-                    <form action="" method="POST">
-                        <input type="hidden" name="id" value="">
-                        <button class="button-delete" type="submit">削除</button>
-                    </form>
-                </div>
-            </li>
-        </ul>
+            <div class="add-form">
+                <form action="" method="POST" id="input-form">
+                    <input type="text" name="title" placeholder="TODOを入力" required>
+                    <button class="button-add" type="submit" name="add">登録</button>
+                </form>
+            </div>
+            
+            <ul class="TODO-items">
+                <?php foreach ($_SESSION['todos'] as $id => $todo): ?>
+                    <li class="TODO-item">
+                        <div class="TODO-title"><?php echo htmlspecialchars($todo) ?></div>
+                        <div class="nav-container">
+                            <form action="" method="POST">
+                                <input type="hidden" name="id" value="<?php echo htmlspecialchars($id) ?>">
+                                <button class="button-update" type="submit" name="update">編集</button>
+                            </form>
+                            <form action="" method="POST">
+                                <input type="hidden" name="id" value="<?php echo htmlspecialchars($id) ?>">
+                                <button class="button-delete" type="submit" name="delete">削除</button>
+                            </form>
+                        </div>
+                    </li>
+                <?php endforeach ?>
+            </ul>
+        <?php endif ?>
     </main>
 </body>
 </html>
